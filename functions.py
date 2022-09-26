0fdd13656c6f2eeb296b0a36768044340c53d1b3
Saumyajeet Das <111430070+saumyajeet1822@users.noreply.github.com>
2022-09-26 17:36:39 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,14 @@
+""" Syntax for def as follows:-
+
+def function-name(arguments):
+    #function body"""
+
+#functions
+
+def hello():
+    print("hello")
+    print("hello again")
+hello()
+
+#calling function
+hello()
