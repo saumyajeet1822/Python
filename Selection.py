acc1275f50fd6aa04e53d61e63086511cf09800d
Saumyajeet Das <111430070+saumyajeet1822@users.noreply.github.com>
2022-09-26 17:30:17 +0530

    Add files via upload

---
+++ Selection.py
@@ -0,0 +1,10 @@
+#python program to illustrate
+#Seletion statement
+
+num1 = 34
+if(num1>12):
+    print("Num 1 is ok")
+elif(num1>39):
+    print("Num1 is not ok")
+else:
+    print("Num2 is great")
